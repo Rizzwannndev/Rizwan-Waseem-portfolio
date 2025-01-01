3d5b9f94b2d90ec74e7537d5dce165886f80d086
Rizwan Waseem <148436818+Rizzwannndev@users.noreply.github.com>
2025-01-01 19:32:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.navlinks a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('.navlinks a[href="#' + id + '"]').classList.add('active');
+        }
+    });
+};
+
+   // Function to copy text to clipboard
+   function copyToClipboard(text) {
+    const el = document.createElement('textarea'); // Create a textarea element
+    el.value = text; // Set the value of the textarea to the specified text
+    el.setAttribute('readonly', ''); // Make it read-only to prevent focus and move outside of view
+    el.style.position = 'absolute';
+    el.style.left = '-9999px'; // Move outside the screen to make it invisible
+
+    document.body.appendChild(el); // Append the textarea element to the document
+    el.select(); // Select the text inside the textarea
+    document.execCommand('copy'); // Copy the selected text to the clipboard
+    document.body.removeChild(el); // Remove the textarea element
+  }
+
+  // Event listener for the element click
+  document.getElementById('copyText').addEventListener('click', function() {
+    const textToCopy = '+92 3122196626'; // Replace this text with your desired text
+    copyToClipboard(textToCopy); // Call the function to copy text to clipboard
+    
+    // Show the styled alert notification
+    const alertDiv = document.createElement('div');
+    alertDiv.textContent = 'Text has been copied to clipboard: ' + textToCopy;
+    alertDiv.classList.add('custom-alert');
+    document.body.appendChild(alertDiv);
+
+    // Remove the alert after 3 seconds
+    setTimeout(function() {
+      alertDiv.remove();
+    }, 3000);
+  });
+
+
